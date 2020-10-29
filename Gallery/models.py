a2eba0a27c4b1377a675e06dfd9db0cf7b4b8da9
Robin Philip <rbnphlp@hotmail.com>
2020-10-29 21:56:37 +0000

    Created Gallery Paintings  - need to add to admin

--- Gallery/models.py
+++ Gallery/models.py
@@ -14,23 +14,10 @@ class Category(models.Model):
 
 
 
-class Painting(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    name = models.CharField(max_length=254)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
-
-    def __str__(self):
-        return self.name
-
 
 
 class Uploaded_Picture(models.Model):
-      category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-      upload_pic = models.ForeignKey('Painting', null=True, blank=True, on_delete=models.SET_NULL)  
+      category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL) 
       name = models.CharField(max_length=254)
       description = models.TextField()
       image_url = models.URLField(max_length=1024, null=True, blank=True)
@@ -44,8 +31,23 @@ class Style_picture(models.Model):
       name = models.CharField(max_length=254)
       description = models.TextField()
       image_url = models.URLField(max_length=1024, null=True, blank=True)
-      image = models.ImageField(null=True, blank=True)
-      style_pic = models.ForeignKey('Painting', null=True, blank=True, on_delete=models.SET_NULL)  
+      image = models.ImageField(null=True, blank=True) 
 
       def __str__(self):
+        return self.name
+
+
+
+class Painting(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    upload_pic = models.ForeignKey('Uploaded_Picture', null=True, blank=True, on_delete=models.SET_NULL) 
+    style_pic = models.ForeignKey('Style_picture', null=True, blank=True, on_delete=models.SET_NULL)  
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
         return self.name
