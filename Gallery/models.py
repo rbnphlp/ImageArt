d6473b7e04cd20558767ffbef6e01c56ea80ff12
Robin Philip <rbnphlp@hotmail.com>
2020-11-11 21:04:59 +0000

    Added upload for images

--- Gallery/models.py
+++ Gallery/models.py
@@ -17,17 +17,17 @@ class Category(models.Model):
 
 class Painting(models.Model):
     "add a checkbox to add to gallery "
-    
+
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    upload_pic = models.ImageField(null=True, blank=True) 
-    style_pic =  models.ImageField(null=True, blank=True) 
+    upload_pic = models.ImageField(null=True, blank=True,upload_to="Gallery_images/Upload_pic/") 
+    style_pic =  models.ImageField(null=True, blank=True,upload_to="Gallery_images/Style_pic/") 
     name = models.CharField(max_length=254,blank=True)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2,blank=True,null=True)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     Add_to_Gallery = models.BooleanField(default=True)
-    upload_style_combined=models.ImageField(null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    upload_style_combined=models.ImageField(null=True, blank=True,upload_to="Gallery_images/Upload_style_combined/")
+    image = models.ImageField(null=True, blank=True,upload_to="Gallery_images/Paintings/")
     
 
     def __str__(self):
--- Gallery/views.py
+++ Gallery/views.py
@@ -11,7 +11,8 @@ from ImageArt.settings import MEDIA_ROOT,MEDIA_URL
 new_width  = 380
 new_height = 512
 
-
+hub_model = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
+                 
 
 def all_paintings(request):
     """ A view to show all paintings, including sorting and search queries """
@@ -59,17 +60,16 @@ def add_painting(request):
             "check if add to gallery is not added  and simply render no saving images"
             add_to_gallery=form.cleaned_data['Add_to_Gallery']
             print(add_to_gallery)
+           
+            "Get save "
+            content_path=MEDIA_ROOT+'/'+request.FILES['upload_pic'].name
+            style_path=MEDIA_ROOT+'/'+request.FILES['style_pic'].name
+            upload_image=load_img(content_path)
+            style_image=load_img(style_path)
+            stylized_image = hub_model(tf.constant(upload_image), tf.constant(style_image))[0]
+            l=tensor_to_image(stylized_image)
             if not add_to_gallery :
-                 " process the  files  and render "
-                 saved_form=form.save()
-                 
-                 hub_model = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
-                 
-                 "Get save "
-                 upload_image=load_img(MEDIA_ROOT+'/'+request.FILES['upload_pic'].name)
-                 style_image=load_img(MEDIA_ROOT+'/'+request.FILES['style_pic'].name)
-                 stylized_image = hub_model(tf.constant(upload_image), tf.constant(style_image))[0]
-                 l=tensor_to_image(stylized_image)
+                
 
                 # Resize image     
                  url_painting=MEDIA_ROOT+'/'+'Gallery_images/Paintings/'+request.FILES['upload_pic'].name
@@ -81,8 +81,17 @@ def add_painting(request):
                     }
 
                  return render(request, template2,context)
+            else: 
+                " process the  files  and render "
+                saved_form=form.save()
+                "Combine style and upload image"
+                content_image_pil = PIL.Image.open(content_path)
+                style_image_pil = PIL.Image.open(style_path)
+             
+                combined_image=get_concat_v_resize(content_image_pil, style_image_pil, resize_big_image=False)
+                combined_image=combined_image.resize((new_width, new_height), PIL.Image.ANTIALIAS)
 
-
+                "Save in form for combined image"  
 
     
 
