7e18b18f7dcaba40895087304e430f80a4bc66c3
Robin Philip <rbnphlp@hotmail.com>
2020-11-21 17:54:38 +0000

    Added basic urls for shopping bag

--- ImageArt/settings.py
+++ ImageArt/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'home',
     'Gallery',
     'crispy_forms',
+    'bag',
 
 ]
 
--- ImageArt/urls.py
+++ ImageArt/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('Gallery/', include('Gallery.urls')),
+    path('bag/',include('bag.urls')),
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
