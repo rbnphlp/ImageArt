22e9ad7d6043b3cfd949dc7bf7cf36f62daeeb2f
Robin Philip <rbnphlp@hotmail.com>
2020-12-03 21:14:29 +0000

    Added stripe elements.js and css

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51HuNpFDLeDN6RAorKOp3WPo6Thq1axUedpa7TAhDTJFgqGcV1kZCvH8L37YTCfieR6xul3FFzJZHARZNuLqai8hn005EaZSQ9o',
+        'client_secret': 'sk_test_51HuNpFDLeDN6RAorIUz2iVVUXMMokqsONDE2Bj4jrTftgSIzm57idROFIS9I2K2YuXW1NNhJTFR5y1CyEToEraWI00pMWvBohG',
     }
 
     return render(request, template, context)
