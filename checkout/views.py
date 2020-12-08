8a23d4118571c8cd80bcbc179ea4e259d186b617
Robin Philip <rbnphlp@hotmail.com>
2020-12-08 21:42:59 +0000

    intent created but not succesful

--- checkout/views.py
+++ checkout/views.py
@@ -6,8 +6,10 @@ from bag.contexts import bag_contents
 from django.conf import settings
 
 import stripe
-
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
@@ -16,13 +18,25 @@ def checkout(request):
     current_bag = bag_contents(request)
     total = current_bag['grand_total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    print(intent)
 
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51HuNpFDLeDN6RAorKOp3WPo6Thq1axUedpa7TAhDTJFgqGcV1kZCvH8L37YTCfieR6xul3FFzJZHARZNuLqai8hn005EaZSQ9o',
-        'client_secret': 'sk_test_51HuNpFDLeDN6RAorIUz2iVVUXMMokqsONDE2Bj4jrTftgSIzm57idROFIS9I2K2YuXW1NNhJTFR5y1CyEToEraWI00pMWvBohG',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
