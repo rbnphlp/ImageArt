a2dd034a2fa7ec22ca26c930c42aef16e52a0745
Robin Philip <rbnphlp@hotmail.com>
2021-01-02 14:13:01 +0000

    changed shopping bag icon to exclude  for delivery as 0

--- home/views.py
+++ home/views.py
@@ -6,4 +6,7 @@ from django.shortcuts import render
 def index(request):
 
     "A view to return undex page"
+    "Add messages when delvery basket has only $2 "
+
+    
     return render(request,'home/index.html')
