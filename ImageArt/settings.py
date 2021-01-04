ff17dd6b86049d03bfc7fd2c8955cac4caadd995
Robin Philip <rbnphlp@hotmail.com>
2021-01-04 21:58:45 +0000

    Added cache control

--- ImageArt/settings.py
+++ ImageArt/settings.py
@@ -147,6 +147,12 @@ else:
 
 
 if 'USE_AWS' in os.environ:
+    
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'imageart'
     AWS_S3_REGION_NAME = 'eu-west-2'
