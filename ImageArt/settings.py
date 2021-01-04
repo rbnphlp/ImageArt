140e7671d13a463da81b4ee6e4e091fe8ad7c0bb
Robin Philip <rbnphlp@hotmail.com>
2021-01-04 22:20:47 +0000

    changes settings to read in AWS settings later

--- ImageArt/settings.py
+++ ImageArt/settings.py
@@ -141,13 +141,19 @@ else:
 
 
 
-#AWS
+#Media files and AWS settings 
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 
 
 if 'USE_AWS' in os.environ:
-    
+
     AWS_S3_OBJECT_PARAMETERS = {
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
         'CacheControl': 'max-age=94608000',
@@ -210,11 +216,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 FREE_DELIVERY_THRESHOLD = 50
