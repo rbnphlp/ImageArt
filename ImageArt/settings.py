bf8855449901a34aa9bcc2ef9a0f516abcc354f6
Robin Philip <rbnphlp@hotmail.com>
2021-01-10 14:17:53 +0000

    Fixed bug to get all  paintings in index.htm

--- ImageArt/settings.py
+++ ImageArt/settings.py
@@ -132,17 +132,8 @@ WSGI_APPLICATION = 'ImageArt.wsgi.application'
 
 DatabaseURL = os.getenv('DATABASE_URL', '')
 
-
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
+DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
     }
 
 
